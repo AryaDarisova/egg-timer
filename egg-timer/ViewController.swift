fdea60a5e26be0524b23c28f5d07e355e7383da0
AryaDarisova <arisova.darya@yandex.ru>
2023-08-01 02:18:18 +0400

    Initial Commit

---
+++ egg-timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egg-timer
+//
+//  Created by Daria Arisova on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
