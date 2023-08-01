3d3e7f2712dfbcc609b0585742d8c92214cfa517
AryaDarisova <arisova.darya@yandex.ru>
2023-08-01 16:48:33 +0400

    Add functions for timer, progress bar and alarm sound

--- egg-timer/ViewController.swift
+++ egg-timer/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = [
+        "Soft": 1,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    var player: AVAudioPlayer?
+    var timer: Timer?
+    var totalTime = 0
+    var passedTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text
+        
+        totalTime = eggTimes[hardness!]! * 60
+        passedTime = 0
+        progressBar.progress = 0.0
+        timer?.invalidate()
+        topLabel.text = "Timer for \(hardness!) started"
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+    }
+    
+    @objc func onTimerFires() {
+        var percentageProgress: Float = 0.0
+        
+        passedTime += 1
+        percentageProgress = Float(passedTime) / Float(totalTime)
+        progressBar.progress = percentageProgress
+        
+        print("Percent: \(percentageProgress)")
+     
+        if passedTime == totalTime {
+            timer?.invalidate()
+            timer = nil
+            topLabel.text = "DONE!"
+            alarmBell()
+        }
+    }
+    
+    func alarmBell() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
 
-
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 }
 
